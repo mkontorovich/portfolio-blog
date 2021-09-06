69b22774a69ba34c5cf11717d09b58741293dd11
Mikhail Kontorovich <mikhail.kontorovich@gmail.com>
2021-09-05 21:48:14 -0500

    fixed naming issues

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,7 +7,7 @@ class PostList(generic.ListView):
     Return all posts that are with status 1 (published) and order from the latest one.
     """
     queryset = Post.objects.filter(status=1).order_by('-created_at')
-    template_name = 'index.html'
+    template_name = 'blog_posts.html'
 
 
 class PostDetail(generic.DetailView):
